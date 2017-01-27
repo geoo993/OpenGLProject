e31e9c6fa92a494eb6a02b3db9e1269278383446
GEORGE QUENTIN <ggeoo93@gmail.com>
2017-01-27 01:23:42 +0000

    project setup

---
+++ OpenGLProject/OpenGLProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGLProject
+//
+//  Created by GEORGE QUENTIN on 26/01/2017.
+//  Copyright © 2017 GEORGE QUENTIN. All rights reserved.
+//
+
+#include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
